6b7a4b6f656943ceb0aaa75f7abe5f8b1e60079b
Yaroslav <doctor355@yahoo.com>
2019-04-28 12:17:50 +0300

    Add styles for login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { Form, Button, Caption, Container } from './LoginStyles';
 const Login = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
+  <Form>
+    <Caption>
+      <h1>autorisation</h1>
+    </Caption>
+    <Container>
+      <Button>Login</Button>
+    </Container>
+  </Form>
 );
 export default Login;
---
+++ src/Components/LoginStyles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  margin: 15% auto;
+  border: 3px solid #f1f1f1;
+  max-width: 400px;
+  max-width: 400px;
+`;
+export const Button = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+`;
+export const Caption = styled.div`
+  text-align: center;
+  margin: 24px 0 12px 0;
+  text-transform: uppercase;
+`;
+export const Container = styled.div`
+  padding: 16px;
+`;
